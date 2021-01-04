fc81a799725c2da975f965546bcbf56f4492dc4d
WonJin <jason7655@naver.com>
2021-01-04 18:46:18 +0900

    delete visual_studio_code

--- visual_studio_code/test_1.py
+++
@@ -1,6 +0,0 @@
-def solution(participant, completion):
-    for i in participant :
-        if participant.count(i) != completion.count(i) :
-            return i
-
-print(solution(['leo', 'kiki', 'eden'],['eden', 'kiki']))
