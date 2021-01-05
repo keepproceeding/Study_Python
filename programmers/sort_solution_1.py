18cba8e389fc420291047284e2ddd4c39ff73ccb
WonJin <jason7655@naver.com>
2021-01-05 23:48:47 +0900

    sort_solution_1

---
+++ programmers/sort_solution_1.py
@@ -0,0 +1,8 @@
+
+# 프로그래머스 LEVEL 2 가장 큰 수 문제 _other code★★★
+# 정렬 알고리즘
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
