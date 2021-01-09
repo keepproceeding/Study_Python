04571f86011e5605c4fa0492df6d0b8df1d5dc02
WonJin <jason7655@naver.com>
2021-01-09 16:26:38 +0900

    stack_greedy_solution

---
+++ programmers/stack_greedy_solution.py
@@ -0,0 +1,12 @@
+# 프로그래머스 LEVEL 2 큰수 만들기 문제 _ other good code
+# stack의 활용성
+def solution(number, k):
+    stack = [number[0]]
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
